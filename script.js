fcc106775f3aaf73c61812804bf663438bc97831
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2020-07-01 00:40:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@
 (function() {
     'use strict';
 //TODO: find a better way to apply themes without constantly running this function and only running it once the items have loaded
-let i = 0
     let intie = setInterval(run=>{document.querySelectorAll(".messages-3amgkR").forEach(instance=>{instance.style.backgroundColor = "#302848"})
 document.querySelectorAll(".members-1998pB").forEach(instance=>{instance.style.backgroundColor = "#302848"})
 document.querySelectorAll(".listContent-2_qb-y").forEach(instance=>{instance.style.backgroundColor = "#302848"})
@@ -30,10 +29,23 @@ document.querySelectorAll(".scroller-2FKFPG").forEach(instance=>{instance.style.
 document.querySelectorAll(".searchBar-6Kv8R2").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
 document.querySelectorAll(".privateChannels-1nO12o").forEach(instance=>{instance.style.backgroundColor = "#2821401"})
 document.querySelectorAll(".container-16j22k").forEach(instance=>{instance.style.backgroundColor = "#302848"})
-
- if(i==200){
- clearInterval(intie)
-}
-                                 },500)
+document.querySelectorAll(".grid-1nZz7S").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".jumpToPresentBar-9P20AM").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".container-1r6BKw").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
+document.querySelectorAll(".scroller-9moviB").forEach(instance=>{instance.style.backgroundColor = "#302848"})
+document.querySelectorAll(".detailsBlock-FoDTGA").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".premiumSubscriptionAccountCredit-25i0tQ").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".nowPlayingColumn-2sl4cE").forEach(instance=>{instance.style.backgroundColor = "#302848"})
+document.querySelectorAll(".scroller-1JbKMe").forEach(instance=>{instance.style.backgroundColor = "#302848"})
+document.querySelectorAll(".scroller-2FKFPG").forEach(instance=>{instance.style.backgroundColor = "#302848"})
+document.querySelectorAll(".actionButton-uPB8Fs").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".wrapper-3D2qGf").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".section-2gLsgF").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
+document.querySelectorAll(".searchBarComponent-32dTOx").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
+document.querySelectorAll(".directionColumn-35P_nr").forEach(instance=>{instance.style.backgroundColor = "#221E36"})
+document.querySelectorAll(".body-3iLsc4").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
+document.querySelectorAll(".footer-1fjuF6").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
+document.querySelectorAll(".quickMessage-2XpSaN").forEach(instance=>{instance.style.backgroundColor = "#342C4D"})
+                                 },20)
 
 })();
